d2f6b58f095e9a9f0e25bdb74eba912cc1c687c1
Osborn Mensah <vicariousptolemy@gmail.com>
2024-06-19 14:50:58 +0000

    committed

---
+++ InheritanceAndMethodOverriding/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace InheritanceAndMethodOverriding
+{
+    class Animal
+    {
+        // add virtual keyword to allow for method overriding
+        public virtual void MakeSound() {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            // animal sound
+            animal.MakeSound();
+
+            // dog sound
+            dog.MakeSound();
+
+            // cat sound
+            cat.MakeSound();
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
